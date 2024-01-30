05353970c5b0ec627bcc814fe1be4a74f1b1cf51
HeoYongmin <snrn2634@naver.com>
2024-01-30 17:15:39 +0900

    2일차 마무리

---
+++ day02/test15_output.py
@@ -0,0 +1,46 @@
+# file : test15_output.py
+# desc : 콘솔 출력 포맷양식 String Format
+
+string_1 = '{}'.format(10) # {}위치에 함수 뒤에 들어있는 값(숫자든 문자든 상관없음)을 치환, 원하는 양식으로 표현 
+print(type(string_1))
+
+name = '허용민' #input('이름 입력 > ')
+string_2 = '안녕하세요, {}입니다!'.format(name)
+print(string_2)
+
+string_3 = '{} {} {}'.format(4000, '만', '빌려주세요')
+print(string_3)
+# 정수를 문자열 포맷
+#=: 기호와 숫자를 분리, 05: 다섯자리를 만들 때 빈자리를 0으로 채우기, d: 정수
+string_4 = '_____{:05d}_____'.format(100)
+print(string_4)
+#실수 문자열 포맷f (기본 소수점6자리)
+# 7.2f는 전체 자리수를 7자리로, 소수점 뒤는 2자리 fix
+string_5 = '_____{:7.2f}______'.format(78.33333333)
+print(string_5)
+
+# 파이썬 3.6 이후 도입. format() 함수를 아예 사용안함
+# 이걸 더 사용
+val = 78.3333333333
+string_6 = f'_____{val:7.2f}______'
+print(string_6)
+
+string_7 = 'Hello, World!'
+print(string_7.upper()) # upper case 대문자 변환
+print(string_7.lower()) # 모두 소문자로
+print(string_7.lower().capitalize()) # capitalize 첫번째 글자만 대문자로 변환
+
+print(string_7.split(',')) # 특정한 단어로 자르는 함수
+
+string_8 = 'Hello, I am YM Heo. I am a stydent. Good Luck for your day.'
+words = string_8.split(' ')
+print(words)
+print(f'string_8 문장 단어 갯수는 {len(words)}개 입니다.')
+
+string_9 = 'A10'
+print(string_9.isalnum()) # True
+print(string_9.isnumeric()) # False A 알파벳
+string_10 = '-10.5' # 소수점은 함수를 만들어서 처리해야...
+print(string_10.isdecimal()) # False
+
+print('안냥' in '안녕하세요') # 문장안에 단어가 있는지 확인
---
+++ day02/test16_times_table.py
@@ -0,0 +1,14 @@
+# file: test16_times_table.py
+# desc : 구구단 프로그램
+# spec: for문 잘 써야함. 2중 for문의 이해
+# debugging
+# F9(중단점 토글), F5(디버깅 시작), F10(한줄씩 실행), F11(함수안으로 진입)
+# shift + F5(디버깅 종료)
+# 조사식을 확인
+# x x y = x*y
+print('구구단 시작!')
+for x in range(2, 3+1): # 2부터 9까지 반복
+    print(f'{x}단 ----->')
+    for y in range(1, 9+1): # 1부터 9까지 반복
+        print(f'{x} x {y} = {x*y:2d}',end = ' ') # end는 엔터 대신 공백으로 변경
+    print() # 안쪽 for문이 끝나면 마지막 엔터를 하나 추가
